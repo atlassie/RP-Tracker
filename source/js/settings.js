a4a7f92f31ee784cc2ad2504deb7b0fa7e2404a7
Dawn Eggleton <dawn.eggleton@Dawns-MacBook-Air.local>
2024-10-12 20:42:42 -0300

    init

---
+++ source/js/settings.js
@@ -0,0 +1,6 @@
+const deployID = 'AKfycbw2AXcBv3_B5ZmkUKniu4hsh41xfZpJopKizv6pSNHTqLTlHN6hoTaknSfmnqC40NFe';
+const sheetID = `1QDmG6bYesiCpdZEvA4Mt8hf723nCjt5SlEQpxpOgap8`;
+const successMessage = `<p class="fullWidth">Submission successful!</p>
+<button onclick="location.reload();" type="button" class="fullWidth submit">Back to form</button>`;
+const threadTags = ["vital", "priority", "rapidfire", "romantic", "family", "friends", "coworkers"];
+const chartColors = ['#cf86a8', '#84a9d1', '#af88ba', '#85b8b5', '#b6aa79', '#c9986e', '#84ad85', '#ba7878'];
